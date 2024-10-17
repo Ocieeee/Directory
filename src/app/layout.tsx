6126a0fe0b62a4e83acbedf5967edb3cf54d565d
Om chaudhari <omchaudhari0003@gmail.com>
2024-10-17 21:48:39 +0530

    Setting up Hono API , Creating Auth API

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Inter } from "next/font/google"
-
+import { Inter } from "next/font/google";
+import { QueryProvider } from "@/components/query-provider";
 import { cn } from "@/lib/utils";
 
-const inter = Inter({subsets: ["latin"]})
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,10 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={cn(inter.className,"antialiased min-h-screen")}
-      >
-        {children}
+      <body className={cn(inter.className, "antialiased min-h-screen")}>
+        <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
   );
--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -24,16 +24,16 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { registerSchema } from "../schemas";
+import { useRegister } from "../api/use-register";
 
-const formSchema = z.object({
-  name: z.string().min(1, "Required"),
-  email: z.string().email(),
-  password: z.string().min(8, "Minimum 8 Character required"),
-});
+
 
 const SignUpCard = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const { mutate } = useRegister()
+
+  const form = useForm<z.infer<typeof registerSchema>>({
+    resolver: zodResolver(registerSchema),
     defaultValues: {
       name: "",
       email: "",
@@ -41,8 +41,8 @@ const SignUpCard = () => {
     },
   });
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+  const onSubmit = (values: z.infer<typeof registerSchema>) => {
+    mutate({json: values});
   };
 
   return (
